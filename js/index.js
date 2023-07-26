14695f650b376417966b18e6c72cfeff72c26065
Keppprogramming <zenasamarash@gmail.com>
2023-07-26 19:17:54 +0200

    1

--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
-function pokaz_chowaj_div_pod_button(el) {
-    var element = document.getElementById(el);
+function gid(el) {
+    return document.getElementById(el);
+}
+function qSelectorAll(el) {
+    return document.querySelectorAll(el);
+}
+function pokaz_chowaj_div_pod_button(element_open_or_close, el) {
+    var element = gid(el);
+    var closed = gid(element_open_or_close);
     if (element.classList.contains('hidden')) {
-        
+        closed.classList.remove('closed');
     }else {
-        
+        closed.classList.add('closed');
     }
     element.classList.toggle('hidden');
 }
