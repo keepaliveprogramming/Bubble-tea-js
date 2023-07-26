2d520988b163de0e9c3ffa199aae2d63c39dbc68
Keppprogramming <zenasamarash@gmail.com>
2023-07-26 11:22:16 +0200

    0.0.1

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function pokaz_chowaj_div_pod_button(el) {
+    var element = document.getElementById(el);
+    if (element.classList.contains('hidden')) {
+        
+    }else {
+        
+    }
+    element.classList.toggle('hidden');
+}
